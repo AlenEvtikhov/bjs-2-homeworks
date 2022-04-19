de92008c4fda858881818a8e935ca7a5f09743bd
AlenEvtikhov <50318546+AlenEvtikhov@users.noreply.github.com>
2022-04-19 05:42:01 +0300

    arrays task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
-  return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+  return arr.filter((n) => n > 0).filter((n) => n % 3 === 0).map((n) => n * 10);
 
-  return resultArr; // array
 }
